52672388b8b56b81daa5a56fb40ffb37f4d70448
krzysztof <krzysztof.mohr@live.com>
2018-04-10 16:54:30 +0100

    added application scaffolding

---
+++ src/app/services/account.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {Stock} from "./stocks.model";
+
+const defaultBalance: number = 10000;
+
+@Injectable()
+export class AccountService {
+  private _balance: number = defaultBalance;
+  private _cost: number = 0;
+  private _value: number = 0;
+  private _stocks: Stock[] = [];
+
+  get balance(): number { return this._balance; }
+  get cost(): number { return this._cost; }
+  get value(): number { return this._value; }
+  get stocks(): Stock[] { return this._stocks; }
+
+  purchase(stock: Stock) : void {
+    stock = Object.assign({}, stock);
+    if(stock.price < this.balance) {
+      this._balance = this.debit(stock.price, this.balance);
+      stock.cost = stock.price;
+      this._cost = this.credit(stock.price, this.cost);
+      stock.change = 0;
+      this._stocks.push(stock);
+      this.calculateValue();
+    }
+  }
+
+  sell(index: number): void {
+
+  }
+
+  private debit(amount: number, balance: number): number {
+    return (balance * 100 - amount * 100) / 100;
+  }
+
+  private credit(amount: number, balance: number): number {
+    return (balance * 100 + amount * 100) / 100;
+  }
+
+  private calculateValue(): void {
+    this._value = this._stocks
+      .map(stock => stock.price)
+      .reduce((a,b) => a + b, 0);
+  }
+}
